7ea92bec1ad2be27e662c4d0b529b33f20850409
Soo Kiat Ter <tersookiat@Soos-MacBook-Pro.local>
2014-04-04 01:27:06 +0100

    Added test class

---
+++ Man's Best Friend/MBFGitTest.h
@@ -0,0 +1,13 @@
+//
+//  MBFGitTest.h
+//  Man's Best Friend
+//
+//  Created by Soo Kiat Ter on 04/04/2014.
+//  Copyright (c) 2014 Can Can Studio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MBFGitTest : NSObject
+
+@end
---
+++ Man's Best Friend/MBFGitTest.m
@@ -0,0 +1,13 @@
+//
+//  MBFGitTest.m
+//  Man's Best Friend
+//
+//  Created by Soo Kiat Ter on 04/04/2014.
+//  Copyright (c) 2014 Can Can Studio. All rights reserved.
+//
+
+#import "MBFGitTest.h"
+
+@implementation MBFGitTest
+
+@end
